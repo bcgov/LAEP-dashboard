5f98b98a4747117a9bee559b1a21ff976ff57067
Yurchak <Stephanie.Yurchak@gov.bc.ca>
2025-05-22 14:06:15 -0700

    update and move tooltips.r

---
+++ app/R/tooltips.R
@@ -0,0 +1,41 @@
+tooltips = list(
+  Population = HTML("Reference date: July 1, 2021<br><br>Source: Statistics Canada, 2021 Census of Population"),
+
+  `Total jobs` = HTML("Total number of people who said they were employed during the census reference week
+                      in 2021<br><br>Source: Statistics Canada, 2021 Census of Population"),
+
+  `Total income (millions)` = HTML("Estimate for the 2020 calendar<br><br>Source: Statistics Canada, 2021 Census of Population"),
+
+  `Average employment income` = HTML("Estimate for the 2020 calendar year. No adjustments made for full-time or part-time workers.<br><br>Source: Statistics Canada, 2021 Census of Population"),
+
+  `Diversity index` = HTML("The diversity index is an important measure to predict the economic
+                            stability of a region in volatile economic times.<br><br>It measures the standard deviation
+                            of the region's income dependence on each basic income source, compared to the
+                            standard deviation of a region 100% dependent on one source.<br><br>
+                           A region entirely dependent on one income source will have a diversity index of zero. A
+                           region equally dependent on each of the defined income sources will have a diversity index of 100.
+                           In general, the diversity index in B.C. is between 50 and 80."),
+
+  summary_statistics = HTML("The reference years are listed as 2010, 2015, and 2020 because the
+                              economic statistics generated from the census are based on the previous
+                              calendar year of collection.<br><br>The population estimates have an
+                              effective date of July 1, 2011, 2016 and 2021, respectively."),
+
+  top_shares = HTML("This table represents the industries with the highest share of the region's basic income.<br><br>
+                    Basic income refers to income that comes from outside the region in the form of employment
+                    and non-employment income, compared to non-basic income which circulates within the
+                    region and is generated from jobs that provide goods and services to the region.<br><br>
+                    The shares of basic income are measured (opposed to total income) because it describes
+                    which industries the region depends on for income flowing into the region. Without a
+                    healthy basic sector, the non-basic sector would not exist."),
+
+  top_employment = HTML("This is a simple ranking of employment by industry,
+                        irrespective of which portions are basic or non-basic.<br><br>
+                        Note: The tourism employment estimate is a portion of many industries,
+                        as defined by their tourism demand. These portions are not subtracted
+                        from the respective industries and adding tourism to the other sectors would result in
+                        double-counting jobs. Most tourism demand falls under the \"other services\" category.")
+
+
+)
+
